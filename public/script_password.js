54df264ed59d916958fac5ea3c611005dcf4a88d
ar-zadeh <55898847+ar-zadeh@users.noreply.github.com>
2024-01-05 13:45:32 -0500

    Update script_password.js
    set the password

--- public/script_password.js
+++ public/script_password.js
@@ -1,6 +1,6 @@
 function checkPassword() {
     var password = document.getElementById("password").value;
-    var correctPassword = "yourPredefinedPassword"; // Replace with your predefined password
+    var correctPassword = "PSU2023"; // Replace with your predefined password
 
     if (password === correctPassword) {
         window.location.href = "index.html"; // Redirects to index.html if correct
